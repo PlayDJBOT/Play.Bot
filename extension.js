86624a3ec484a3cea8efe6d61d987c5d4da69b27
PlayDJBOT <william.ivan.helmenius@gmail.com>
2015-03-05 15:59:48 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -31,7 +31,7 @@
          }
 
          */
-         bot.commands.baconCommand = {
+         bot.commands.rulesCommand = {
             command: 'rules',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
